faed8eb8981cdef0783f3a54cbc5286d5fcc62cd
aryoprasetya <aryoprasetyagithub@gmail.com>
2025-08-28 19:42:29 +0700

    Delete api directory

--- api/status.js
+++
@@ -1,9 +0,0 @@
-// api/status.js
-export default async function handler(req, res) {
-  const { orderId } = req.query;
-
-  global.payments = global.payments || {};
-  const status = global.payments[orderId] || "Pending";
-
-  res.status(200).json({ status });
-}
