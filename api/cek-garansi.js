2c15e0578c8b104b4555992d417bdcb1c991d480
aryoprasetya <aryoprasetyagithub@gmail.com>
2025-08-28 19:33:26 +0700

    Update cek-garansi.js

--- api/cek-garansi.js
+++ api/cek-garansi.js
@@ -1,19 +1,24 @@
-// api/cek-garansi.js
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, query, where, getDocs } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Konfigurasi Firebase (isi dari Firebase Console)
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  apiKey: "AIzaSyD1AkEsEKa4UGbxNiU3HGsD86fNZ5HoX-M",
+  authDomain: "garansi-prasetya.firebaseapp.com",
+  projectId: "garansi-prasetya",
+  storageBucket: "garansi-prasetya.firebasestorage.app",
+  messagingSenderId: "251914402966",
+  appId: "1:251914402966:web:e41190698f670050d116b7",
+  measurementId: "G-SNCGS0Q73Y"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const analytics = getAnalytics(app);
 
 export default async function handler(req, res) {
   if (req.method !== "POST") {
